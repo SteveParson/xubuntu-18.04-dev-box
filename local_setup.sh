005988064296881fcd667ba02b32157c24d39ffa
Stephen Parson <stephen.parson@canada.ca>
2019-04-16 16:10:40 -0400

    fix: ansible options

--- local_setup.sh
+++ local_setup.sh
@@ -3,17 +3,17 @@
 if [ $(dpkg-query -W -f='${Status}' ansible 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
 
     echo "Adding ansible apt repos"
-    sudo apt-get install software-properties-common
-    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt-get install -y software-properties-common
+    sudo apt-add-repository -y ppa:ansible/ansible
     sudo apt-get update 
  
     echo "Installing Ansible"
-    sudo apt-get install ansible
+    sudo apt-get install -y ansible
 fi
 
 cd provisioning
 echo "Running playbook"
-ansible-playbook -i hosts site.yml --connection=local --extra-vars "user=$USER"
+ansible-playbook -i hosts site.yml --connection=local --extra-vars "user=$USER" -K
 
 echo "Please reboot now"
 
